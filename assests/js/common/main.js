cd667c1aeec0d86f8df589b22d0bcdca2560642e
@meeriit <amritpokharel51@gmail.com>
2023-01-07 21:28:54 +0545

    special vertical fixes

--- assests/js/common/main.js
+++ assests/js/common/main.js
@@ -97,8 +97,9 @@ $('.slider-primary').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   asNavFor: '.slider-main',
-  dots: true,
   centerMode: true,
   focusOnSelect: true,
   vertical: true,
+  verticalSwiping: true,
+  arrows: false,
 });
